46a8d09b3acb3c6f4a66cd52d7aec79b1a463c23
vansinh1501 <vansinht10@gmail.com>
2023-12-20 14:19:43 +0700

    hieu

--- Source.cpp
+++ Source.cpp
@@ -2,12 +2,20 @@
 
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b){
+	return a-b;
+}
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
+	int a , b ;
+	cout << "nhap a";
+	cin >> a;
+	cout<< "nhap b";
+	cin >> b;
+	cout<<"hieu la a-b"<<hieu;
 	system("pause");
 	return 0;
 }
